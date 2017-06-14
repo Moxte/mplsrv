a68b295b8d1710abc0623565fdb4e5c68f1f2774
Moxte <moxte@yahoo.com>
2017-06-14 22:31:27 +0300

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(8080);
 
 app.get('/', function(req, res){
 	res.send('Hey OMG I am ALIVE');
