a835f3af9d48eaf629f212e2d78e0a6ce8f94f63
Moxte <moxte@yahoo.com>
2017-06-14 22:55:11 +0300

    io test

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ server.listen(8080);
 app.get('/', function(req, res){
 	res.send('Hey OMG I am ALIVE');
 });
-
+io.on('connection', function(socket){
+	socket.emit('message', {hello: 'world'});
+	socket.on('message', function(data){
+		console.log(data);
+		});
+	});
+}
 console.log('------ server');
